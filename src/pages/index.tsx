7d9d6dbca03a685de2f7925c62a7032cf334332e
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-22 20:14:31 +0100

    proxies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { Outline } from "@/libs/heroicons"
 import { useAllProxies } from "@/mods/entities/proxy/data"
 import { ProxyRow } from "@/mods/entities/proxy/row"
+import { ReactNode } from "react"
+
+export function SmallShrinkableOppositeAnchor(props: { children: ReactNode } & { "aria-disabled"?: boolean } & JSX.IntrinsicElements["a"]) {
+  const { children, "aria-disabled": disabled = false, ...rest } = props
+
+  return <a className="group po-md bg-opposite text-opposite rounded-xl outline-none aria-[disabled=false]:hover:bg-opposite-hover focus-visible:outline-opposite aria-disabled:opacity-50 transition-opacity"
+    aria-disabled={disabled}
+    {...rest}>
+    <div className="h-full w-full flex items-center justify-center gap-2 group-aria-[disabled=false]:group-active:scale-90 transition-transform">
+      {children}
+    </div>
+  </a >
+}
 
 export default function Home() {
   const proxies = useAllProxies(0)
 
   return <div className="h-full w-full overflow-y-scroll">
-    <div className="h-full w-full m-auto max-w-4xl flex flex-col">
-      <div className="p-8 grow flex flex-col">
+    <div className="h-full w-full m-auto max-w-3xl flex flex-col">
+      <div className="p-4 grow flex flex-col">
         <div className="h-[min(32rem,90dvh)] shrink-0 grow flex flex-col items-center">
           <div className="grow" />
           <h1 className="text-center font-medium text-6xl">
@@ -15,10 +28,41 @@ export default function Home() {
           </h1>
           <div className="h-4" />
           <div className="text-center text-contrast text-xl">
-            Free and unlimited WebSocket-to-TCP proxies for everyone.
+            Free and unlimited WebSocket-to-TCP proxies,
+          </div>
+          <div className="text-center text-contrast text-xl">
+            powered by the Network protocol.
           </div>
           <div className="grow" />
-          <div>
+          <div className="flex items-center">
+            <SmallShrinkableOppositeAnchor
+              href="#proxies">
+              All proxies
+            </SmallShrinkableOppositeAnchor>
+          </div>
+          <div className="grow" />
+        </div>
+        <div className="h-8" />
+        <div className="grid place-items-stretch gap-4 grid-cols-[repeat(auto-fill,minmax(12rem,1fr))]">
+          <InfoCard title="Free"
+            subtitle={`Free and unlimited for any kind of usage.`} />
+          <InfoCard title="Fast"
+            subtitle={`Proxies are incentivized to relay your data.`} />
+          <InfoCard title="Open"
+            subtitle={`Anyone can run a proxy and join the network.`} />
+        </div>
+        <div className="h-[25dvh]" />
+        <div id="proxies" />
+        <div className="h-[25dvh]" />
+        <div className="p-6 min-h-[50dvh] bg-contrast rounded-xl flex flex-col">
+          <h1 className="text-6xl">
+            Proxies
+          </h1>
+          <div className="text-contrast">
+            All registered public proxies
+          </div>
+          <div className="h-8 grow shrink-0" />
+          <div className="">
             {proxies.data?.get()?.map((proxy, i) =>
               <div key={i}>
                 <ProxyRow data={proxy} />
@@ -31,16 +75,14 @@ export default function Home() {
               Add your own
             </a>
           </div>
-          <div className="grow" />
         </div>
-        <div className="h-8" />
-        <div className="grid place-items-stretch gap-4 grid-cols-[repeat(auto-fill,minmax(16rem,1fr))]">
-          <InfoCard title="Free"
-            subtitle={`Free and unlimited for any kind of usage.`} />
-          <InfoCard title="Fast"
-            subtitle={`Nodes are incentivized to relay your data.`} />
-          <InfoCard title="Open"
-            subtitle={`Anyone can use and run nodes.`} />
+        <div className="h-[50dvh]" />
+        <div className="text-center p-4">
+          <a className="hover:underline"
+            target="_blank" rel="noreferrer"
+            href="https://ethbrno.cz">
+            Made by cypherpunks
+          </a>
         </div>
       </div>
     </div>
