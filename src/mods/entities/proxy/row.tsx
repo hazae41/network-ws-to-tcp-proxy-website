85d2ce58244e1d71bbab80dc4170052238dfca9a
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-28 17:14:25 +0100

    fix

--- src/mods/entities/proxy/row.tsx
+++ src/mods/entities/proxy/row.tsx
@@ -180,11 +180,11 @@ export function ProxyRow(props: { data: ProxyData }) {
         if (minimumBigInt > (2n ** 20n))
           throw new Error("Minimum too high")
 
-        const secretZeroHexArray = await generateOrThrow(params)
+        const secretZeroHex = await generateOrThrow(params)
 
         await socketAndCounter.requestOrThrow<string>({
           method: "net_tip",
-          params: [secretZeroHexArray]
+          params: [secretZeroHex]
         }).then(r => BigInt(r.unwrap()))
 
         const tlsHelloBase16 = "16030100a5010000a10303000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f000020cca8cca9c02fc030c02bc02cc013c009c014c00a009c009d002f0035c012000a010000580000001800160000136578616d706c652e756c666865696d2e6e6574000500050100000000000a000a0008001d001700180019000b00020100000d0012001004010403050105030601060302010203ff0100010000120000"
